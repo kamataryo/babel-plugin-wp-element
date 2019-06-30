4ff16a0df18a48ed3ee209504719a2c16508b93d
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-06-30 10:02:01 +0900

    Enhance typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import * as Babel from '@babel/core'
+import * as t from '@babel/types'
+
 export default function() {
   return {
     visitor: {
-      ImportDeclaration(path: any) {
+      ImportDeclaration(path: Babel.NodePath<t.ImportDeclaration>) {
         const { source } = path.node
         if (source.value === 'react' || source.value === 'react-dom') {
           source.value = '@wordpress/element'
